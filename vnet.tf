f9475fc4476acfcf6ce9c33d6d25e3a3d805b83c
Mike King <Mike.King@meijer.com>
2020-10-22 15:37:36 +0000

    updated vnet config

--- vnet.tf
+++ vnet.tf
@@ -1,5 +1,27 @@
+resource "azurerm_resource_group" "example" {
+  name     = "rg-ea2-my-resources"
+  location = "eastus2"
+  tags = {
+    environment = "dev"
+    costcenter  = "it"
+    Department = "devops"
+    Billable = "true"
+  }  
+}
+
 module "network" {
-  source  = "app.terraform.io/meijer-demo-1022/network/azurerm"
-  version = "3.2.1"
-  resource_group_name = "rg-ea2-test"
+  source              = "Azure/network/azurerm"
+  resource_group_name = azurerm_resource_group.example.name
+  address_space       = "10.0.0.0/16"
+  subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  subnet_names        = ["subnet1", "subnet2", "subnet3"]
+
+  tags = {
+    environment = "dev"
+    costcenter  = "it"
+    Department = "devops"
+    Billable = "true"
+  }
+
+  depends_on = [azurerm_resource_group.example]
 }
