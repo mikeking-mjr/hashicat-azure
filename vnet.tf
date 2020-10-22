a76ea023aae6c0459b9724a2cb9db2ffbb499dea
Mike King <Mike.King@meijer.com>
2020-10-22 15:39:25 +0000

    updated vnet config again

--- vnet.tf
+++ vnet.tf
@@ -1,17 +1,6 @@
-resource "azurerm_resource_group" "example" {
-  name     = "rg-ea2-my-resources"
-  location = "eastus2"
-  tags = {
-    environment = "dev"
-    costcenter  = "it"
-    Department = "devops"
-    Billable = "true"
-  }  
-}
-
 module "network" {
   source              = "Azure/network/azurerm"
-  resource_group_name = azurerm_resource_group.example.name
+  resource_group_name = "rg-ea2-mk-test"
   address_space       = "10.0.0.0/16"
   subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   subnet_names        = ["subnet1", "subnet2", "subnet3"]
@@ -22,6 +11,4 @@ module "network" {
     Department = "devops"
     Billable = "true"
   }
-
-  depends_on = [azurerm_resource_group.example]
 }
