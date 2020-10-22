eea689db4f2fcf9b6d48326b9873e00c5ae2c113
Mike King <Mike.King@meijer.com>
2020-10-22 15:49:01 +0000

    updated vnet config again

--- vnet.tf
+++ vnet.tf
@@ -1,5 +1,5 @@
 module "network" {
-  source  = "app.terraform.io/russshearer-training/network/azurerm"
+  source  = "app.terraform.io/meijer-demo-1022/network/azurerm"
   version = "3.0.1"
   # insert required variables here
   resource_group_name = azurerm_resource_group.myresourcegroup.name
